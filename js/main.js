7a7aba31e662e78aa4ce717a59f3e5de1363d59d
dimagong <dimagong@gmail.com>
2020-07-20 02:15:44 +0300

    added burger with mooving

--- js/main.js
+++ js/main.js
@@ -9,3 +9,21 @@
         }
     }
 }());
+
+
+//burger handler
+
+(function () {
+    const birgerItem = document.querySelector('.burger');
+    const menu = document.querySelector('.header-nav');
+    const menuCloseItem = document.querySelector('.header__nav-close');
+
+    birgerItem.addEventListener('click', () => {
+        menu.classList.add('header-nav-active');
+    });
+    menuCloseItem.addEventListener('click', () => {
+        menu.classList.remove('header-nav-active');
+    });
+
+}())
+
